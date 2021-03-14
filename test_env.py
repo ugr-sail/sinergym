a7a5a137634c74c03468896acbb9927c9d8cf04c
jajimer <javi.j21@gmail.com>
2021-03-14 19:50:18 +0100

    3 weather files included: Hot dry (AZ), mixed humid (NY) and cool marine (WA)

--- test_env.py
+++ test_env.py
@@ -3,7 +3,7 @@ import energym
 import numpy as np
 
 env = gym.make('Eplus-discrete-v1')
-for i in range(9):
+for i in range(1):
     obs = env.reset()
     rewards = []
     done = False
@@ -13,5 +13,7 @@ for i in range(9):
         obs, reward, done, info = env.step(a)
         rewards.append(reward)
         k += 1
+        if k % 10000 == 0:
+            print(k, sum(rewards))
     print('Episode ', i, 'Mean reward: ', np.mean(rewards), 'Cumulative reward: ', sum(rewards))
 env.close()
