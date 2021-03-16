39759aeadb89d1633387f7890dae0cf747d24ee1
Antonio <antomanjavacas@gmail.com>
2021-03-16 11:29:05 +0100

    Discrete environment improvements

--- test_env.py
+++ test_env.py
@@ -7,13 +7,13 @@ for i in range(1):
     obs = env.reset()
     rewards = []
     done = False
-    k = 0
+    current_month = '1'
     while not done:
         a = env.action_space.sample()
         obs, reward, done, info = env.step(a)
         rewards.append(reward)
-        k += 1
-        if k % 5000 == 0:
-            print(k, reward, info)
+        if info['month'] != current_month: # display results every month
+            current_month = info['month']
+            print('Reward: ', reward, info)
     print('Episode ', i, 'Mean reward: ', np.mean(rewards), 'Cumulative reward: ', sum(rewards))
 env.close()
