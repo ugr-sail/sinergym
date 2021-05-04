ac248621ed5969630e855db274a0119287f25a11
Alejandro <alejandroac79@gmail.com>
2021-05-04 13:38:01 +0200

    Updated env tests and added tests for CSVLogger's - close #28

--- tests/test_env.py
+++ tests/test_env.py
@@ -3,6 +3,7 @@ from random import randint
 import gym
 import energym
 import os
+import csv
 
 def test_reset(env_demo):
 	obs=env_demo.reset()
@@ -16,8 +17,9 @@ def test_step(env_demo):
 	assert len(obs)==19
 	assert type(reward)!=None
 	assert not done
-	assert list(info.keys())==['timestep', 'day', 'month', 'hour', 'total_power', 'total_power_no_units', 'comfort_penalty', 'temperature', 'out_temperature']
-	assert info['timestep']==env_demo.simulator._eplus_run_stepsize *1
+	assert list(info.keys())==['timestep', 'time_elapsed', 'day', 'month', 'hour', 'total_power', 'total_power_no_units', 'comfort_penalty', 'temperature', 'out_temperature']
+	assert info['timestep']==1
+	assert info['time_elapsed']==env_demo.simulator._eplus_run_stepsize *info["timestep"]
 
 	action=randint(0, 9)
 	obs, reward, done, info = env_demo.step(action)
@@ -25,8 +27,9 @@ def test_step(env_demo):
 	assert len(obs)==19
 	assert type(reward)!=None
 	assert not done
-	assert list(info.keys())==['timestep', 'day', 'month', 'hour', 'total_power', 'total_power_no_units', 'comfort_penalty', 'temperature', 'out_temperature']
-	assert info['timestep']==env_demo.simulator._eplus_run_stepsize *2
+	assert list(info.keys())==['timestep', 'time_elapsed', 'day', 'month', 'hour', 'total_power', 'total_power_no_units', 'comfort_penalty', 'temperature', 'out_temperature']
+	assert info['timestep']==2
+	assert info['time_elapsed']==env_demo.simulator._eplus_run_stepsize *info["timestep"]
 
 	#This is a discrete environment, action cannot be a tuple with continuous values
 	with pytest.raises(KeyError):
@@ -36,6 +39,25 @@ def test_close(env_demo):
 	env_demo.close()
 	assert not env_demo.simulator._episode_existed
 	assert env_demo.simulator._conn==None
+
+def test_loggers(env_demo):
+	loggers=[env_demo.logger_progress,env_demo.logger_monitor]
+
+	#Check CSV's have been created and linked in simulator correctly
+	assert loggers[0].log_file==env_demo.simulator._env_working_dir_parent+"/progress.csv"
+	assert loggers[1].log_file==env_demo.simulator._eplus_working_dir+"/monitor.csv"
+
+	for logger in loggers:
+
+		assert os.path.isfile(logger.log_file)
+	
+
+		#Check headers
+		with open(logger.log_file, mode="r", newline='') as csvfile:
+			reader=csv.reader(csvfile, delimiter=',')
+			for row in reader:
+				assert ','.join(row)+'\n'==logger.header
+				break
 
 def test_all_environments():
 	
