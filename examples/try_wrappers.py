53c458fb870b031ca41e2318237dd5fdf3470ecd
Alejandro <alejandroac79@gmail.com>
2021-06-01 13:05:21 +0200

    Rename files that start with test word but they aren't test to be executed by pytest - close #42

--- examples/try_wrappers.py
+++ examples/try_wrappers.py
@@ -18,8 +18,9 @@ for i in range(1):
         a = env.action_space.sample()
         obs, reward, done, info = env.step(a)
         rewards.append(reward)
-        if info['month'] != current_month: # display results every month
+        if info['month'] != current_month:  # display results every month
             current_month = info['month']
             print('Reward: ', sum(rewards), info)
-    print('Episode ', i, 'Mean reward: ', np.mean(rewards), 'Cumulative reward: ', sum(rewards))
+    print('Episode ', i, 'Mean reward: ', np.mean(
+        rewards), 'Cumulative reward: ', sum(rewards))
 env.close()
