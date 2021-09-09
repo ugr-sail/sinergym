c8e06a49c313415779061343787dfc2d1291d44d
AlejandroCN7 <alejandroac79@gmail.com>
2021-09-09 13:42:02 +0200

    Release action repared, changed version to 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     reqs = f.read().splitlines()
 
 setup(name='energym',
-      version='1.2.0',
+      version='1.2.1',
       install_requires=reqs,
       include_package_data=True,
       extras_require={
