1fd2728d06489d8f278040932eb3fa36c52b4a6a
AlejandroCN7 <alejandroac79@gmail.com>
2021-09-10 14:05:23 +0200

    Added gcloud functions code doc and added functionality to detect machines in different instance groups

--- gcloud.py
+++ gcloud.py
@@ -7,88 +7,131 @@ import googleapiclient.discovery
 #from six.moves import input
 
 
-def list_instances(service, project, zone):
-    peticion = service.instances().list(project=project, zone=zone)
-    print(peticion.to_json())
+def list_instances(service, project, zone, base_instances_names=None):
+    """List instances names created in Google Cloud currently.
+
+    Args:
+        service: gcloud API service built previously
+        project: Project id from Google Cloud Platform
+        zone: Google Cloud Zone where instances are
+        base_instances_names: By default is None, it filters instances if contains that sub str or not.
+
+    Returns:
+        list(str): Name of the instances availables in Google Cloud.
+
+    """
+    service.instances().list(project=project, zone=zone)
     result = service.instances().list(project=project, zone=zone).execute()
-    return result['items'] if 'items' in result else None
+    instance_objects = result['items'] if 'items' in result else None
+    instances = []
+    if instance_objects:
+        instances = [instance['name'] for instance in instance_objects]
+    if base_instances_names:
+        instances = [
+            instance for instance in instances if base_instances_names in instance]
+    return instances
 
 
-def create_instance(service, project, zone, name, bucket):
-    # Get the latest Debian Jessie image.
-    image_response = service.images().getFromFamily(
-        project='debian-cloud', family='debian-9').execute()
-    source_disk_image = image_response['selfLink']
+# def create_instance(service, project, zone, name, bucket):
+#     """Create an individual instance in Google Cloud.
 
-    # Configure the machine
-    machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
-    startup_script = open(
-        os.path.join(
-            os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
-    image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
-    image_caption = "Ready for dessert?"
+#     Args:
+#         service: gcloud API service built previously
+#         project: Project id from Google Cloud Platform
+#         zone: Google Cloud Zone where instance will be
+#         name: Name of the new instance
+#         bucket:
 
-    config = {
-        'name': name,
-        'machineType': machine_type,
+#     Returns:
+#         list(str): Name of the instances availables in Google Cloud.
 
-        # Specify the boot disk and the image to use as a source.
-        'disks': [
-            {
-                'boot': True,
-                'autoDelete': True,
-                'initializeParams': {
-                    'sourceImage': source_disk_image,
-                }
-            }
-        ],
+#     """
+#     # Get the latest Debian Jessie image.
+#     image_response = service.images().getFromFamily(
+#         project='debian-cloud', family='debian-9').execute()
+#     source_disk_image = image_response['selfLink']
 
-        # Specify a network interface with NAT to access the public
-        # internet.
-        'networkInterfaces': [{
-            'network': 'global/networks/default',
-            'accessConfigs': [
-                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
-            ]
-        }],
+#     # Configure the machine
+#     machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
+#     startup_script = open(
+#         os.path.join(
+#             os.path.dirname(__file__), 'startup-script.sh'), 'r').read()
+#     image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
+#     image_caption = "Ready for dessert?"
 
-        # Allow the instance to access cloud storage and logging.
-        'serviceAccounts': [{
-            'email': 'default',
-            'scopes': [
-                'https://www.googleapis.com/auth/devstorage.read_write',
-                'https://www.googleapis.com/auth/logging.write'
-            ]
-        }],
+#     config = {
+#         'name': name,
+#         'machineType': machine_type,
 
-        # Metadata is readable from the instance and allows you to
-        # pass configuration from deployment scripts to instances.
-        'metadata': {
-            'items': [{
-                # Startup script is automatically executed by the
-                # instance upon startup.
-                'key': 'startup-script',
-                'value': startup_script
-            }, {
-                'key': 'url',
-                'value': image_url
-            }, {
-                'key': 'text',
-                'value': image_caption
-            }, {
-                'key': 'bucket',
-                'value': bucket
-            }]
-        }
-    }
+#         # Specify the boot disk and the image to use as a source.
+#         'disks': [
+#             {
+#                 'boot': True,
+#                 'autoDelete': True,
+#                 'initializeParams': {
+#                     'sourceImage': source_disk_image,
+#                 }
+#             }
+#         ],
 
-    return service.instances().insert(
-        project=project,
-        zone=zone,
-        body=config).execute()
+#         # Specify a network interface with NAT to access the public
+#         # internet.
+#         'networkInterfaces': [{
+#             'network': 'global/networks/default',
+#             'accessConfigs': [
+#                 {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+#             ]
+#         }],
+
+#         # Allow the instance to access cloud storage and logging.
+#         'serviceAccounts': [{
+#             'email': 'default',
+#             'scopes': [
+#                 'https://www.googleapis.com/auth/devstorage.read_write',
+#                 'https://www.googleapis.com/auth/logging.write'
+#             ]
+#         }],
+
+#         # Metadata is readable from the instance and allows you to
+#         # pass configuration from deployment scripts to instances.
+#         'metadata': {
+#             'items': [{
+#                 # Startup script is automatically executed by the
+#                 # instance upon startup.
+#                 'key': 'startup-script',
+#                 'value': startup_script
+#             }, {
+#                 'key': 'url',
+#                 'value': image_url
+#             }, {
+#                 'key': 'text',
+#                 'value': image_caption
+#             }, {
+#                 'key': 'bucket',
+#                 'value': bucket
+#             }]
+#         }
+#     }
+
+#     return service.instances().insert(
+#         project=project,
+#         zone=zone,
+#         body=config).execute()
 
 
 def delete_instance(service, project, zone, name):
+    """Delete an instance inner Google Cloud.
+
+    Args:
+        service: gcloud API service built previusly
+        project: Project id from Google Cloud Platform
+        zone: Google Cloud Zone where instance is
+        name: The name of instance you want to delete
+
+    Returns:
+        str JSON: State response from API
+
+    """
     return service.instances().delete(
         project=project,
         zone=zone,
@@ -102,6 +145,20 @@ def create_instance_group(
         size,
         template_name,
         group_name):
+    """Create an instance group (MIG) in Google Cloud.
+
+    Args:
+        service: gcloud API service built previusly
+        project: Project id from Google Cloud Platform
+        zone: Google Cloud Zone where instances are
+        size: Number of instances desired inner MIG
+        template_name: template name for machine type definition, previously defined into your Google Cloud account.
+        group_name: Base name for every machine inner MIG, this name will be concatenated with a different random str for every machine
+
+    Returns:
+        str JSON: State response from API
+
+    """
 
     body_request = {
         "versions": [
@@ -120,18 +177,18 @@ def create_instance_group(
 
 def main(project, zone):
     service = googleapiclient.discovery.build('compute', 'v1')
-    print('Creating instance group.')
-    size = 4
+    size = 2
     template_name = "energym-template"
-    group_name = "group-example"
-    create_instance_group(
-        service,
-        project,
-        zone,
-        size,
-        template_name,
-        group_name)
-    # instances = list_instances(service, project, zone)
+    group_name = "sinergym"
+    # create_instance_group(
+    #     service,
+    #     project,
+    #     zone,
+    #     size,
+    #     template_name,
+    #     group_name)
+    instances = list_instances(service, project, zone, "sinergym")
+    print(instances)
     # print('Instances in project %s and zone %s:' % (project, zone))
     # for instance in instances:
     #     print(' - ' + instance['name'])
