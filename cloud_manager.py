932c8ff0ea46a62d1fc5f2df0c28feb7717ea8a0
AlejandroCN7 <alejandroac79@gmail.com>
2021-09-21 13:29:20 +0200

    Fixed some bugs for gcloud API use, such as unsyncronizwed tags, pretty prints, etc

--- cloud_manager.py
+++ cloud_manager.py
@@ -1,4 +1,6 @@
 import argparse
+from time import sleep
+from pprint import pprint
 import energym.utils.gcloud as gcloud
 
 parser = argparse.ArgumentParser(
@@ -40,34 +42,76 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-n_experiments = len(args.commands)
-
+print('Init Google cloud service API...')
 service = gcloud.init_gcloud_service()
 
 # Create instance group
-gcloud.create_instance_group(
+n_experiments = len(args.commands)
+print('Creating instance group(MIG) for experiments ({} instances)...'.format(
+    n_experiments))
+response = gcloud.create_instance_group(
     service=service,
     project=args.project,
     zone=args.zone,
     size=n_experiments,
     template_name=args.template_name,
     group_name=args.group_name)
+pprint(response)
 
-# Problem: VM is up earlier than container executing --> sleep execution?
+# Wait for the machines to be fully created.
+print(
+    '{0} status is {1}.'.format(
+        response['operationType'],
+        response['status']))
+if response['status'] != 'DONE':
+    response = gcloud.wait_for_operation(
+        service,
+        args.project,
+        args.zone,
+        operation=response['id'],
+        operation_type=response['operationType'])
+pprint(response)
+print('MIG created.')
 
 # List VM names
-instances = gcloud.list_instances(
-    service=service,
-    project=args.project,
-    zone=args.zone,
-    base_instances_names=args.group_name)
+print('Looking for instance names... (waiting for they are visible too)')
+# Sometimes, although instance group insert status is DONE, isn't visible
+# for API yet. Hence, we have to wait for with a loop...
+instances = []
+while len(instances) < n_experiments:
+    instances = gcloud.list_instances(
+        service=service,
+        project=args.project,
+        zone=args.zone,
+        base_instances_names=args.group_name)
+    sleep(3)
+print(instances)
 # Number of machines should be the same than commands
-assert len(instances) == n_experiments
 
 # Execute a comand in every container inner VM
+print('Sending commands to every container VM... (waiting for container inner VM is ready too)')
 for i, instance in enumerate(instances):
+    container_id = None
+    # Obtain container id inner VM
+    while not container_id:
+        container_id = gcloud.get_container_id(instance_name=instance)
+        sleep(5)
+    # Execute command in container
     gcloud.execute_remote_command_instance(
+        container_id=container_id,
         instance_name=instance,
         experiment_command=args.commands[i])
+    print(
+        'command {} has been sent to ionstance {}(container: {}).'.format(
+            args.commands[i],
+            instance,
+            container_id))
 
+print('All VM\'s are working correctly, see Google Cloud Platform Console.')
 # Close VM when finished with google cloud alerts?
+
+# python  cloud_manager.py --project_id energym-314709
+# --experiments_commands
+# 'python 3 DRL_battery.py --environment Eplus-5Zone-hot-discrete-v1 --episodes 3 --algorithm DQN --logger log_interval 1 --seed 54'
+# 'python3 DRL_battery.py --environment Eplus-5Zone-hot-continuous-stochastic-v1 --episodes 3 --algorithm PPO --logger --log_interval 1 --tensorboard --normalization --seed 54'
+# --template_name energym_template --group_name sinergym-vm
