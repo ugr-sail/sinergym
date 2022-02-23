f4fdb418340048d8f8e278af1c6a2dbfeb2c2c81
AlejandroCN7 <alejandroac79@gmail.com>
2022-02-23 14:58:07 +0200

    [Gym env] First approximation to dinamic observation/action space (errors still) and reestructuration for code

--- sinergym/envs/eplus_env.py
+++ sinergym/envs/eplus_env.py
@@ -37,7 +37,7 @@ class EplusEnv(gym.Env):
         discrete_actions: bool = True,
         weather_variability: Optional[Tuple[float]] = None,
         reward: Any = LinearReward(),
-        config_params: Optional[Dict[str, Any]] = None
+        config_params: Dict[str, Any] = {}
     ):
         """Environment with EnergyPlus simulator.
 
@@ -50,8 +50,12 @@ class EplusEnv(gym.Env):
             discrete_actions (bool, optional): Whether the actions are discrete (True) or continuous (False). Defaults to True.
             weather_variability (Optional[Tuple[float]], optional): Tuple with sigma, mu and tao of the Ornstein-Uhlenbeck process to be applied to weather data. Defaults to None.
             reward (Any, optional): Reward function instance used for agent feedback. Defaults to LinearReward().
-            config_params (Optional[Dict[str, Any]], optional): Dictionary with all extra configuration for simulator. Defaults to None.
+            config_params (Dict[str, Any], optional): Dictionary with all extra configuration for simulator. Defaults to empty Dict.
         """
+
+        # ---------------------------------------------------------------------------- #
+        #                               PATHS DEFINITION                               #
+        # ---------------------------------------------------------------------------- #
         eplus_path = os.environ['EPLUS_PATH']
         bcvtb_path = os.environ['BCVTB_PATH']
         self.pkg_data_path = pkg_resources.resource_filename(
@@ -65,60 +69,125 @@ class EplusEnv(gym.Env):
         self.spaces_path = os.path.join(
             self.pkg_data_path, 'variables', spaces_file)
 
+        # ---------------------------------------------------------------------------- #
+        #                             SIMULATOR DEFINITION                             #
+        # ---------------------------------------------------------------------------- #
         self.simulator = EnergyPlus(
             env_name=env_name,
             eplus_path=eplus_path,
             bcvtb_path=bcvtb_path,
             idf_path=self.idf_path,
             weather_path=self.weather_path,
-            variable_path=self.variables_path,
+            variables_path=self.variables_path,
             config_params=config_params
         )
 
-        # parse variables (observation and action) from cfg file
-        self.variables = parse_variables(self.variables_path)
+        # ---------------------------------------------------------------------------- #
+        #                               OTHERS ATTRIBUTES                              #
+        # ---------------------------------------------------------------------------- #
 
         # Random noise to apply for weather series
         self.weather_variability = weather_variability
+        # Action space flag
+        self.flag_discrete = discrete_actions
+        # Reward class
+        self.cls_reward = reward
+        # ONLY FOR CONTINUOUS ACTION
+        self.setpoints_space = None
 
-        # parse observation and action spaces from spaces_path
+        # ---------------------------------------------------------------------------- #
+        #                      VARIABLES ATTRIBUTE FOR ENVIRONMENT                     #
+        # ---------------------------------------------------------------------------- #
+        # By default, self.variables has variables.cfg path values
+        self.variables = parse_variables(self.variables_path)
+        # Changed by custom variables if they are specified
+        if config_params.get('observation_variables'):
+            self.variables['observation'] = config_params['observation_variables']
+        if config_params.get('action_variables'):
+            self.variables['action'] = config_params['action_variables']
+
+        # ---------------------------------------------------------------------------- #
+        #                       DEFAULT ACTION/OBSERVATION SPACE                       #
+        # ---------------------------------------------------------------------------- #
+        # Parse observation and action spaces from spaces_path
         space = parse_observation_action_space(self.spaces_path)
         observation_def = space['observation']
         discrete_action_def = space['discrete_action']
         continuous_action_def = space['continuous_action']
 
-        # Observation space
-        self.observation_space = gym.spaces.Box(
-            low=observation_def[0],
-            high=observation_def[1],
-            shape=observation_def[2],
-            dtype=observation_def[3])
+        # ---------------------------------------------------------------------------- #
+        #                         CUSTOM OBSERVATION DEFINITION                        #
+        # ---------------------------------------------------------------------------- #
 
-        # Action space
-        self.flag_discrete = discrete_actions
-
-        # Discrete
-        if self.flag_discrete:
-            self.action_mapping = discrete_action_def
-            self.action_space = gym.spaces.Discrete(len(discrete_action_def))
-        # Continuous
+        # Custom observation variables has been specified in config_params
+        if config_params.get('observation_space'):
+            self.observation_space = config_params['observation_space']
+        # Else Default environment Observation space
         else:
-            # Defining action values setpoints (one per value)
-            self.action_setpoints = []
-            for i in range(len(self.variables['action'])):
-                # action_variable --> [low,up]
-                self.action_setpoints.append([
-                    continuous_action_def[0][i], continuous_action_def[1][i]])
+            self.observation_space = gym.spaces.Box(
+                low=observation_def[0],
+                high=observation_def[1],
+                shape=observation_def[2],
+                dtype=observation_def[3])
 
-            self.action_space = gym.spaces.Box(
-                # continuous_action_def[2] --> shape
-                low=np.repeat(-1, continuous_action_def[2][0]),
-                high=np.repeat(1, continuous_action_def[2][0]),
-                dtype=continuous_action_def[3]
-            )
+        # ---------------------------------------------------------------------------- #
+        #                           CUSTOM ACTION DEFINITION                           #
+        # ---------------------------------------------------------------------------- #
+        # Custom action variables has been specified in config_params
+        if config_params.get('action_space'):
+            if isinstance(config_params['action_space'], gym.spaces.Box):
+                # Normalize action_space to [-1,1] in all variables and save
+                # original
+                self.setpoints_space = config_params['action_space']
+                self.action_space = gym.spaces.Box(low=np.repeat(-1,
+                                                                 len(self.variables['action'])),
+                                                   high=np.repeat(1,
+                                                                  len(self.variables['action'])),
+                                                   dtype=np.float32)
+            elif self.flag_discrete and not isinstance(config_params['action_space'], gym.spaces.Box):
+                self.action_space = config_params['action_space']
+                self.action_mapping = config_params['action_mapping']
+            else:
+                raise RuntimeError('Custom action specification has no sense.')
 
-        # Reward class
-        self.cls_reward = reward
+        # Else Default environment action space
+        else:
+            # Discrete
+            if self.flag_discrete:
+                self.action_mapping = discrete_action_def
+                self.action_space = gym.spaces.Discrete(
+                    len(discrete_action_def))
+            # Continuous
+            else:
+                # Defining action values setpoints (one per value)
+                for i in range(len(self.variables['action'])):
+                    # action_variable --> [low,up]
+                    # CAMBIAAAAAAR
+                    self.action_setpoints.append([
+                        continuous_action_def[0][i], continuous_action_def[1][i]])
+
+                self.action_space = gym.spaces.Box(
+                    # continuous_action_def[2] --> shape
+                    low=np.repeat(-1, continuous_action_def[2][0]),
+                    high=np.repeat(1, continuous_action_def[2][0]),
+                    dtype=continuous_action_def[3]
+                )
+
+    def reset(self) -> np.ndarray:
+        """Reset the environment.
+
+        Returns:
+            np.ndarray: Current observation.
+        """
+        # Change to next episode
+        time_info, obs, done = self.simulator.reset(self.weather_variability)
+        obs_dict = dict(zip(self.variables['observation'], obs))
+
+        obs_dict['day'] = time_info[0]
+        obs_dict['month'] = time_info[1]
+        obs_dict['hour'] = time_info[2]
+
+        return np.array(list(obs_dict.values()), dtype=np.float32)
 
     def step(self,
              action: Union[int,
@@ -199,22 +268,6 @@ class EplusEnv(gym.Env):
 
         return np.array(list(obs_dict.values()),
                         dtype=np.float32), reward, done, info
-
-    def reset(self) -> np.ndarray:
-        """Reset the environment.
-
-        Returns:
-            np.ndarray: Current observation.
-        """
-        # Change to next episode
-        time_info, obs, done = self.simulator.reset(self.weather_variability)
-        obs_dict = dict(zip(self.variables['observation'], obs))
-
-        obs_dict['day'] = time_info[0]
-        obs_dict['month'] = time_info[1]
-        obs_dict['hour'] = time_info[2]
-
-        return np.array(list(obs_dict.values()), dtype=np.float32)
 
     def render(self, mode: str = 'human') -> None:
         """Environment rendering.
