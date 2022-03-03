9f3fd87bdd86d2ff1c6516061d1948e5248db0d2
AlejandroCN7 <AlejandroCN7@users.noreply.github.com>
2022-03-03 09:49:55 +0000

    Automated format fixes (autopep8 + isort)

--- sinergym/utils/controllers.py
+++ sinergym/utils/controllers.py
@@ -2,9 +2,9 @@
 from datetime import datetime
 from typing import Any, List, Optional, Sequence, Tuple
 
-from ..utils.common import parse_variables, get_season_comfort_range
-
 from numpy import arange
+
+from ..utils.common import get_season_comfort_range, parse_variables
 
 
 class RandomController(object):
@@ -64,16 +64,19 @@ class RuleBasedController(object):
 
         season_comfort_range = get_season_comfort_range(month, day)
 
-        if out_temp not in arange(season_comfort_range[0], season_comfort_range[1], .1):
-            if hour in range(6, 18): # day
+        if out_temp not in arange(
+                season_comfort_range[0], season_comfort_range[1], .1):
+            if hour in range(6, 18):  # day
                 action = (19.44, 25.0)
-            elif hour in range(18, 22): # evening
+            elif hour in range(18, 22):  # evening
                 action = (20.0, 24.44)
-            else: # night
+            else:  # night
                 action = (18.33, 23.33)
-        else: # maintain setpoints if comfort requirements are already met
-            current_cool_setpoint = obs_dict['Zone Thermostat Cooling Setpoint Temperature (SPACE1-1)']
-            current_heat_setpoint = obs_dict['Zone Thermostat Heating Setpoint Temperature (SPACE1-1)']
+        else:  # maintain setpoints if comfort requirements are already met
+            current_cool_setpoint = obs_dict[
+                'Zone Thermostat Cooling Setpoint Temperature (SPACE1-1)']
+            current_heat_setpoint = obs_dict[
+                'Zone Thermostat Heating Setpoint Temperature (SPACE1-1)']
             action = (current_heat_setpoint, current_cool_setpoint)
 
         return action
