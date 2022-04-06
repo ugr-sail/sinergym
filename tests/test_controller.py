1a873f936010e8bd917b63ca96950dfc9f5fc82f
Antonio <antomanjavacas@gmail.com>
2022-04-06 09:53:13 +0300

    Fixed controllers' tests

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,6 +1,6 @@
 import pytest
 
-from sinergym.utils.controllers import RBC5Zone, RBCDatacenter
+from sinergym.utils.controllers import RBC5Zone
 
 
 @pytest.mark.parametrize(
@@ -17,22 +17,6 @@ from sinergym.utils.controllers import RBC5Zone, RBCDatacenter
 def test_rule_based_controller_5Zone(env_name, request):
     env = request.getfixturevalue(env_name)
     rule_based_agent = RBC5Zone(env)
-    obs = env.reset()
-
-    for i in range(3):
-        action = rule_based_agent.act(obs)
-        assert isinstance(action, tuple)
-        for value in action:
-            assert value is not None
-        obs, reward, done, info = env.step(action)
-
-        assert tuple(info['action_']) == action
-
-    env.close()
-
-def test_rule_based_controller_Datacenter(env_name, request):
-    env = request.getfixturevalue(env_name)
-    rule_based_agent = RBCDatacenter(env)
     obs = env.reset()
 
     for i in range(3):
