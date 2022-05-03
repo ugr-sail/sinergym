331fa190afcaeef46439e7e797e6995724420c4e
alejandrocn7 <alejandroac79@gmail.com>
2022-05-03 12:53:45 +0300

    revert try_env.py and added test.py in order to check new functionality separately

--- try_env.py
+++ try_env.py
@@ -2,21 +2,8 @@ import gym
 import numpy as np
 
 import sinergym
-from sinergym.utils.wrappers import LoggerWrapper
 
-extra_conf = {
-    'action_space': {
-        'ThermostatSetpoint:DualSetpoint': [{
-            'name': 'Space1-DualSetP-RL',
-            'heating_name': 'Space1-HtgSetP-RL',
-            'cooling_name': 'Space1-ClgSetP-RL',
-            'zones': ['SPACE1-1']
-        }]
-    }
-}
-env = gym.make('Eplus-demo-v1')
-env.action_space = gym.spaces.Box(low=0, high=0, shape=(0,))
-env = LoggerWrapper(env)
+env = gym.make('Eplus-5Zone-hot-continuous-v1')
 for i in range(1):
     obs = env.reset()
     rewards = []
