2cd257ecc82155ccd0b5e0fc2e0fb6b4aa09c6e5
AlejandroCN7 <alejandroac79@gmail.com>
2023-02-20 14:30:22 +0000

    Added tests for common.is_wrapped and common.unwrap_wrapper

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,7 +3,39 @@ import shutil
 import pytest
 from opyplus import Epm, WeatherData
 
+from sinergym.utils.wrappers import NormalizeObservation
 import sinergym.utils.common as common
+
+
+def test_unwrap_wrapper(
+        env_demo_continuous,
+        env_wrapper_normalization,
+        env_all_wrappers):
+    # Check if env_wrapper_normalization unwrapped is env_demo_continuous
+    assert not hasattr(env_demo_continuous, 'unwrapped_observation')
+    assert hasattr(env_all_wrappers, 'unwrapped_observation')
+    assert hasattr(env_wrapper_normalization, 'unwrapped_observation')
+    assert hasattr(env_wrapper_normalization, 'env')
+    env = common.unwrap_wrapper(
+        env_wrapper_normalization,
+        NormalizeObservation)
+    assert not hasattr(env, 'unwrapped_observation')
+    # Check if trying unwrap a not wrapped environment the result is None
+    env = common.unwrap_wrapper(
+        env_demo_continuous,
+        NormalizeObservation)
+    assert env is None
+    env = common.unwrap_wrapper(env_all_wrappers, NormalizeObservation)
+    assert not hasattr(env, 'unwrapped_observation')
+
+
+def test_is_wrapped(
+        env_demo_continuous,
+        env_wrapper_normalization,
+        env_all_wrappers):
+    assert not common.is_wrapped(env_demo_continuous, NormalizeObservation)
+    assert common.is_wrapped(env_wrapper_normalization, NormalizeObservation)
+    assert common.is_wrapped(env_all_wrappers, NormalizeObservation)
 
 
 @pytest.mark.parametrize(
