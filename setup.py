2cd763afea7657523e36dbfa4dd5e80af6af82bc
AlejandroCN7 <alejandroac79@gmail.com>
2023-03-22 14:00:03 +0000

    Added stable-baselines3 gymnasium support branch in extra dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='sinergym',
           'extras': [
               'matplotlib',  # visualization
               # DRL with pytorch
-              'stable-baselines3',
+              'stable-baselines3 @ git+https://github.com/DLR-RM/stable-baselines3@feat/gymnasium-support',
               'wandb',
               'pytest',
               'pytest-cov',
@@ -47,11 +47,11 @@ setup(name='sinergym',
               'pytest',
               'pytest-cov',
               'pytest-xdist',
-              'stable-baselines3',
+              'stable-baselines3 @ git+https://github.com/DLR-RM/stable-baselines3@feat/gymnasium-support',
               'wandb'
           ],
           'DRL': [
-              'stable-baselines3',
+              'stable-baselines3 @ git+https://github.com/DLR-RM/stable-baselines3@feat/gymnasium-support',
               'wandb'
           ],
           'doc': [
