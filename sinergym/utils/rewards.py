6ada9d660c9c5e598b234ddb4066851498efa8f2
AlejandroCN7 <alejandroac79@gmail.com>
2023-07-07 14:02:42 +0000

    Reward: Fix obs_dict key name (day is now day_of_month)

--- sinergym/utils/rewards.py
+++ sinergym/utils/rewards.py
@@ -120,7 +120,7 @@ class LinearReward(BaseReward):
         """
 
         month = obs_dict['month']
-        day = obs_dict['day']
+        day = obs_dict['day_of_month']
         year = obs_dict['year']
         current_dt = datetime(int(year), int(month), int(day))
 
