3a52aa4d76014f0c319bf884b612da8b76406b25
AlejandroCN7 <AlejandroCN7@users.noreply.github.com>
2023-11-09 09:15:47 +0000

    Automated format fixes (autopep8 + isort)

--- scripts/load_agent.py
+++ scripts/load_agent.py
@@ -4,13 +4,13 @@ import sys
 
 import gymnasium as gym
 import numpy as np
+import wandb
 from gymnasium.wrappers.normalize import NormalizeReward
 from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC, TD3
 from stable_baselines3.common.monitor import Monitor
 
 import sinergym
 import sinergym.utils.gcloud as gcloud
-import wandb
 from sinergym.utils.constants import *
 from sinergym.utils.rewards import *
 from sinergym.utils.wrappers import *
