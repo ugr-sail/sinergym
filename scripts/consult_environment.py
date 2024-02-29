f5226343197b5027cbfa365fba27c9463e217512
AlejandroCN7 <alejandroac79@gmail.com>
2024-02-29 14:38:25 +0000

    Creating functionality to consult all Sinergym avilable IDS and generating an example script in scripts/consult_environment.py

---
+++ scripts/consult_environment.py
@@ -0,0 +1,11 @@
+import sinergym
+import gymnasium as gym
+from sinergym.utils.common import get_ids
+
+# Get the list of available environments
+sinergym_environment_ids = get_ids()
+print(sinergym_environment_ids)
+
+# Make and consult some of the environments
+env = gym.make('Eplus-5zone-hot-continuous-stochastic-v1')
+print(env.info())
