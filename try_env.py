5fd7363631ddeaaa4d6d6d64b231aafd73e3a64e
AlejandroCN7 <alejandroac79@gmail.com>
2024-06-17 15:21:38 +0000

    Updated try_env.py script with new features and variables to reduce

--- try_env.py
+++ try_env.py
@@ -9,63 +9,54 @@ from sinergym.utils.wrappers import (
     IncrementalWrapper,
     RoundActionWrapper,
     ExtremeFlowControlWrapper,
+    HeatPumpEnergyWrapper,
     ReduceObservationWrapper)
 
 # Creating environment and applying wrappers for normalization and logging
 env = gym.make(
-    'Eplus-radiant_pump_flow_heating-stockholm-continuous-stochastic-v1')
+    'Eplus-radiant_free_heating-stockholm-continuous-stochastic-v1')
 # env = RoundActionWrapper(env)
+env = HeatPumpEnergyWrapper(env)
+env = NormalizeObservation(env)
 env = ExtremeFlowControlWrapper(env)
 env = NormalizeAction(env)
-env = NormalizeObservation(env)
 env = LoggerWrapper(env)
-env = ReduceObservationWrapper(env, obs_reduction=[
-    'radiant_hvac_outlet_temperature_living',
-    'radiant_hvac_outlet_temperature_kitchen',
-    'radiant_hvac_outlet_temperature_bed1',
-    'radiant_hvac_outlet_temperature_bed2',
-    'radiant_hvac_outlet_temperature_bed3',
-    'surface_internal_source_location_temperature_living',
-    'surface_internal_source_location_temperature_kitchen',
-    'surface_internal_source_location_temperature_bed1',
-    'surface_internal_source_location_temperature_bed2',
-    'surface_internal_source_location_temperature_bed3',
-    'surface_internal_user_specified_location_temperature_living',
-    'surface_internal_user_specified_location_temperature_kitchen',
-    'surface_internal_user_specified_location_temperature_bed1',
-    'surface_internal_user_specified_location_temperature_bed2',
-    'surface_internal_user_specified_location_temperature_bed3',
-    'people_occupant_living',
-    'people_occupant_kitchen',
-    'people_occupant_bed1',
-    'people_occupant_bed2',
-    'people_occupant_bed3',
-    'thermal_comfort_mean_radiant_temperature_living',
-    'thermal_comfort_mean_radiant_temperature_kitchen',
-    'thermal_comfort_mean_radiant_temperature_bed1',
-    'thermal_comfort_mean_radiant_temperature_bed2',
-    'thermal_comfort_mean_radiant_temperature_bed3',
-    'thermal_comfort_clothing_value_living',
-    'thermal_comfort_clothing_value_kitchen',
-    'thermal_comfort_clothing_value_bed1',
-    'thermal_comfort_clothing_value_bed2',
-    'thermal_comfort_clothing_value_bed3',
-    'thermal_comfort_fanger_model_ppd_living',
-    'thermal_comfort_fanger_model_ppd_kitchen',
-    'thermal_comfort_fanger_model_ppd_bed1',
-    'thermal_comfort_fanger_model_ppd_bed2',
-    'thermal_comfort_fanger_model_ppd_bed3',
-    'people_air_temperature_living',
-    'people_air_temperature_kitchen',
-    'people_air_temperature_bed1',
-    'people_air_temperature_bed2',
-    'people_air_temperature_bed3',
-    'aquarea9kw_plr_performance_curve_output_value',
-    'eeraquarea9kw_performance_curve_output_value',
-    'coolcapaquarea9kw_performance_curve_output_value',
-    'heat_pump_load_side_heat_transfer_rate',
-    'heat_pump_load_side_mass_flow_rate'
-])
+env = ReduceObservationWrapper(
+    env,
+    obs_reduction=[
+        'radiant_hvac_outlet_temperature_living',
+        'radiant_hvac_outlet_temperature_kitchen',
+        'radiant_hvac_outlet_temperature_bed1',
+        'radiant_hvac_outlet_temperature_bed2',
+        'radiant_hvac_outlet_temperature_bed3',
+        'radiant_hvac_inlet_temperature_living',
+        'radiant_hvac_inlet_temperature_kitchen',
+        'radiant_hvac_inlet_temperature_bed1',
+        'radiant_hvac_inlet_temperature_bed2',
+        'radiant_hvac_inlet_temperature_bed3',
+        'surface_internal_user_specified_location_temperature_living',
+        'surface_internal_user_specified_location_temperature_kitchen',
+        'surface_internal_user_specified_location_temperature_bed1',
+        'surface_internal_user_specified_location_temperature_bed2',
+        'surface_internal_user_specified_location_temperature_bed3',
+        'people_occupant_living',
+        'people_occupant_kitchen',
+        'people_occupant_bed1',
+        'people_occupant_bed2',
+        'people_occupant_bed3',
+        'flow_rate_living',
+        'flow_rate_kitchen',
+        'flow_rate_bed1',
+        'flow_rate_bed2',
+        'flow_rate_bed3',
+        'heat_source_load_side_heat_transfer_rate',
+        'heat_source_load_side_mass_flow_rate',
+        'crf',
+        'heat_cap_mod',
+        'cop_plr_mod',
+        'cop_temp_mod',
+        'heat_cap',
+        'plr_current'])
 
 # Execute interactions during 3 episodes
 for i in range(1):
