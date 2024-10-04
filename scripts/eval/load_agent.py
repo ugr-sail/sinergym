(v3.6.3) - Normalization calibration improvement and load_agent.py fix (#451)
* scripts/eval/load_agent.py: fix bug in example script (random agent loop instead of using loaded model)

* Normalization calibration save: latest calibration saved in rootoutput folder and intermediate states in episode folders

* LoggerEvalCallback: Normalization calibration saved in evaluation folder (if normalization wrapper is applied)

* Documentation: Indicate this changes in normalization wrapper

* Updated Sinergym version from 3.6.2 to 3.6.3

scripts/eval/load_agent.py
@@ -91,7 +91,7 @@ try:
                 # parse str parameters to sinergym Callable or Objects if it is
                 # required
                 if isinstance(value, str):
-                    if '.' in value:
+                    if '.' in value and '.txt' not in value:
                         parameters[name] = eval(value)
             env = wrapper_class(env=env, ** parameters)
 
@@ -152,25 +152,13 @@ try:
     for i in range(conf['episodes']):
         # Reset the environment to start a new episode
         obs, info = env.reset()
-        rewards = []
         truncated = terminated = False
-        current_month = 0
         while not (terminated or truncated):
-            # Random action control
-            a = env.action_space.sample()
+            # Use the agent to predict the next action
+            a, _ = model.predict(obs, deterministic=True)
             # Read observation and reward
             obs, reward, terminated, truncated, info = env.step(a)
-            rewards.append(reward)
-            # If this timestep is a new month start
-            if info['month'] != current_month:  # display results every month
-                current_month = info['month']
-                # Print information
-                logger.info('Reward: {}'.format(sum(rewards)))
-                logger.info('Info: {}'.format(info))
-                # Final episode information print
-                logger.info(
-                    'Episode {} - Mean reward: {} - Cumulative Reward: {}'.format(
-                        i, np.mean(rewards), sum(rewards)))
+
     env.close()
 
     # ---------------------------------------------------------------------------- #

sinergym/utils/wrappers.py
@@ -287,21 +287,25 @@ class NormalizeObservation(gym.Wrapper):
 
     def reset(self, **kwargs):
         """Resets the environment and normalizes the observation."""
+
+        # Update normalization calibration if it is required
+        if self.get_wrapper_attr('episode') > 0:
+            self._save_normalization_calibration()
+
         obs, info = self.env.reset(**kwargs)
 
         # Save original obs in class attribute
         self.unwrapped_observation = deepcopy(obs)
 
-        # Update normalization calibration if it is required
-        self._save_normalization_calibration()
-
         return self.normalize(np.array([obs]))[0], info
 
     def close(self):
-        """Close the environment and save normalization calibration."""
-        self.env.close()
+        """save normalization calibration and close the environment."""
         # Update normalization calibration if it is required
-        self._save_normalization_calibration()
+        if self.get_wrapper_attr('episode') > 0:
+            self._save_normalization_calibration()
+
+        self.env.close()
 
     # ----------------------- Wrapper extra functionality ----------------------- #
 
@@ -337,7 +341,12 @@ class NormalizeObservation(gym.Wrapper):
         """
         self.logger.info(
             'Saving normalization calibration data.')
-        # Save in txt in output folder
+        # Save in txt in episode output folder
+        np.savetxt(fname=self.get_wrapper_attr(
+            'episode_path') + '/mean.txt', X=self.mean)
+        np.savetxt(fname=self.get_wrapper_attr(
+            'episode_path') + '/var.txt', X=self.var)
+        # Overwrite output root folder mean and var as latest calibration
         np.savetxt(fname=self.get_wrapper_attr(
             'workspace_path') + '/mean.txt', X=self.mean)
         np.savetxt(fname=self.get_wrapper_attr(