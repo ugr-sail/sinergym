d23e49d644850709a756922640e515ca0d616857
AlejandroCN7 <alejandroac79@gmail.com>
2025-01-29 11:45:38 +0000

    Common: Fix weather variability parsing from configurations

--- sinergym/utils/common.py
+++ sinergym/utils/common.py
@@ -386,16 +386,13 @@ def convert_conf_to_env_parameters(
         if weather_variability:
 
             # Cast weather variability variation from list to tuple
-            for variable, variation in weather_variability.items():
-                weather_variability[variable] = tuple(variation)
-                # Check that the weather variability variation is a tuple of
-                # three elements
-                try:
-                    assert len(weather_variability[variable]) == 3
-                except AssertionError as err:
-                    logger.critical(
-                        'Weather variability variation in {} must be a tuple of three elements (sigma, mu and tao of OU process)'.format(variable))
-                    raise err
+            weather_variability = {
+                var_name: tuple(
+                    tuple(param) if isinstance(param, list) else param
+                    for param in var_params
+                )
+                for var_name, var_params in weather_variability.items()
+            }
 
             id = 'Eplus-' + conf['id_base'] + '-' + \
                 weather_id + '-continuous-stochastic-v1'
