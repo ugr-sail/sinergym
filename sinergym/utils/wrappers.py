(v3.8.1) - Normalization fix (#480)
* Wrappers: Fix NormalizeObservation Wrapper calibration

* Simulator: Simulator back to sinergym v3.7.12

* Fix Devcontainer copy sinergym folder

* Update Poetry dependencies (stable baselines 3 2.6.0 instead of 2.6.0a2)

* Update Sinergym version from 3.8.0 to 3.8.1

* Improve eplus.py style

* Fix pytype in eplus.py

* Fix simulator test

sinergym/utils/wrappers.py
@@ -281,7 +281,9 @@ class NormalizeObservation(gym.Wrapper):
         self.unwrapped_observation = None
 
         # Initialize normalization calibration
-        self.obs_rms = RunningMeanStd(shape=self.observation_space.shape)
+        self.obs_rms = RunningMeanStd(
+            shape=self.observation_space.shape,
+            dtype=self.observation_space.dtype)
 
         # Set mean and variance
         self.obs_rms.mean = self._process_metric(
@@ -397,8 +399,10 @@ class NormalizeObservation(gym.Wrapper):
         """Normalizes the observation using the running mean and variance of the observations.
         If automatic_update is enabled, the running mean and variance will be updated too."""
         if self.automatic_update:
-            self.obs_rms.update(obs)
+            # Update running statistics
+            self.obs_rms.update(np.array([obs], dtype=np.float32))
 
+        # Calculate normalized observation
         std = np.sqrt(self.obs_rms.var + self.epsilon)
         return (obs - self.obs_rms.mean) / std
 