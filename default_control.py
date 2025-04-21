9c73f65b53565b9dace6efdac643c1e6771efee3
AlejandroCN7 <alejandroac79@gmail.com>
2025-04-21 08:38:13 +0000

    add default control and controller in try_env.py

--- default_control.py
+++ default_control.py
@@ -2,7 +2,6 @@ import logging
 
 import gymnasium as gym
 import numpy as np
-from sinergym.utils.controllers import RBCIncremental5Zone
 
 import sinergym
 from sinergym.utils.logger import TerminalLogger
@@ -21,13 +20,16 @@ logger = terminal_logger.getLogger(
 )
 
 # Create environment and apply wrappers for normalization and logging
-env = gym.make('Eplus-5zone-cool-continuous-stochastic-v1')
+env = gym.make('Eplus-5zone-cool-continuous-stochastic-v1',
+               actuators={},
+               action_space=gym.spaces.Box(
+                   low=0,
+                   high=0,
+                   shape=(0,)))
 # env = NormalizeAction(env)
 # env = NormalizeObservation(env)
 env = LoggerWrapper(env)
 env = CSVLogger(env)
-
-agent = RBCIncremental5Zone(env)
 
 # Execute 3 episodes
 for i in range(10):
@@ -40,7 +42,7 @@ for i in range(10):
     while not (terminated or truncated):
 
         # Random action selection
-        a = agent.act(obs)
+        a = env.action_space.sample()
 
         # Perform action and receive env information
         obs, reward, terminated, truncated, info = env.step(a)
