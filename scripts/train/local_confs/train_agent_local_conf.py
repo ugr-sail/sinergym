c74e048aecced06554116ee9d0d62c4009540b67
AlejandroCN7 <alejandroac79@gmail.com>
2025-05-23 20:48:10 +0000

    Scripts: Train local script improved

--- scripts/train/local_confs/train_agent_local_conf.py
+++ scripts/train/local_confs/train_agent_local_conf.py
@@ -7,13 +7,14 @@ import gymnasium as gym
 import numpy as np
 import wandb
 import yaml
-from stable_baselines3 import *
 from stable_baselines3 import __version__ as sb3_version
 from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.logger import HumanOutputFormat
 from stable_baselines3.common.logger import Logger as SB3Logger
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.noise import NormalActionNoise
+
+from sinergym.utils.wrappers import WandBLogger
 
 import sinergym
 import sinergym.utils.gcloud as gcloud
@@ -22,13 +23,16 @@ from sinergym.utils.common import (
     is_wrapped,
     process_algorithm_parameters,
     process_environment_parameters,
+    create_environment,
+    import_from_path,
+    deep_update
 )
 from sinergym.utils.logger import WandBOutputFormat
-from sinergym.utils.common import apply_wrappers_info, get_wrappers_info
 
 # ---------------------------------------------------------------------------- #
 #                             Parameters definition                            #
 # ---------------------------------------------------------------------------- #
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '--configuration',
@@ -39,7 +43,6 @@ parser.add_argument(
     help='Path to experiment configuration (YAML file)'
 )
 args = parser.parse_args()
-# ------------------------------------------------------------------------------#
 
 # ---------------------------------------------------------------------------- #
 #                             Read yaml parameters                             #
@@ -48,123 +51,35 @@ args = parser.parse_args()
 with open(args.configuration, 'r') as yaml_conf:
     conf = yaml.safe_load(yaml_conf)
 
+
+# ---------------------------------------------------------------------------- #
+#                            Train script execution                            #
+# ---------------------------------------------------------------------------- #
+
 try:
     # ---------------------------------------------------------------------------- #
     #                               Register run name                              #
     # ---------------------------------------------------------------------------- #
     experiment_date = datetime.today().strftime('%Y-%m-%d_%H-%M')
-    experiment_name = conf['algorithm']['name'] + '-' + conf['environment'] + \
-        '-episodes-' + str(conf['episodes'])
-    if conf.get('id'):
-        experiment_name += '-id-' + str(conf['id'])
-    experiment_name += '_' + experiment_date
-
-    # ---------------------------------------------------------------------------- #
-    #                           Environment construction                           #
-    # ---------------------------------------------------------------------------- #
-    env_params = {}
-
-    # Update env params configuration with env yaml file if exists
-    if conf.get('env_yaml_config'):
-        with open(conf['env_yaml_config'], 'r') as env_yaml_conf:
-            env_params.update(yaml.load(env_yaml_conf, Loader=yaml.FullLoader))
-
-    # Update env params configuration with specified env parameters if exists
-    if conf.get('env_params'):
-        env_params.update(process_environment_parameters(conf['env_params']))
-
-    # For this script, the execution name will be updated
-    env_params.update({'env_name': experiment_name})
-
-    # Build environment
-    env = gym.make(
-        conf['environment'],
-        ** env_params)
-
-    # env for evaluation if enabled
-    eval_env = None
-    if conf.get('evaluation'):
-        eval_name = conf['evaluation'].get(
-            'name', env.get_wrapper_attr('name') + '-EVAL')
-        env_params.update({'env_name': eval_name})
-        eval_env = gym.make(
-            conf['environment'],
-            ** env_params)
-
-    # ---------------------------------------------------------------------------- #
-    #                                   Wrappers                                   #
-    # ---------------------------------------------------------------------------- #
-    wrappers = {}
-    # Read wrappers from yaml file if exists
-    if conf.get('wrappers_yaml_config'):
-        with open(conf['wrappers_yaml_config'], 'r') as f:
-            wrappers = yaml.load(f, Loader=yaml.FullLoader)
-
-    # Else read wrappers from configuration file if exists
-    elif conf.get('wrappers'):
-        wrappers = conf['wrappers']
-        for wrapper_class_name, wrapper_arguments in wrappers.items():
-            for name, value in wrapper_arguments.items():
-                # parse str parameters to sinergym Callable or Objects if
-                # required
-                if isinstance(value, str):
-                    if '.' in value:
-                        wrapper_arguments[name] = eval(value)
-
-    # Apply wrappers to environment
-    if wrappers:
-        env = apply_wrappers_info(env, wrappers)
-        # Write wrappers configuration to yaml file
-        get_wrappers_info(env)
-
-        if eval_env is not None:
-            key_to_remove = [
-                key for key in wrappers if 'WandBLogger' in key][0]
-            del wrappers[key_to_remove]
-            eval_env = apply_wrappers_info(eval_env, wrappers)
-            # Write wrappers configuration to yaml file
-            get_wrappers_info(eval_env)
-
-    # ---------------------------------------------------------------------------- #
-    #                           Defining model (algorithm)                         #
-    # ---------------------------------------------------------------------------- #
-    alg_name = conf['algorithm']['name']
-    alg_params = conf['algorithm'].get(
-        'parameters', {'policy': 'MlpPolicy'})
-    alg_params = process_algorithm_parameters(alg_params)
-
-    # --------------------------- Training from scratch -------------------------- #
-    if conf.get('model') is None:
-        try:
-            model = eval(alg_name)(env=env, ** alg_params)
-        except NameError:
-            raise NameError(
-                'Algorithm {} does not exists. It must be a valid SB3 algorithm.'.format(alg_name))
-
-        # Register hyperparameters in wandb if wrapped
-        if is_wrapped(env, WandBLogger):
-            experiment_params = {
-                'sinergym-version': sinergym.__version__,
-                'python-version': sys.version,
-                'stable-baselines3-version': sb3_version
-            }
-            experiment_params.update(conf)
-            env.get_wrapper_attr('wandb_run').config.update(experiment_params)
-
-    # ------------------------ Training from a given model ----------------------- #
+    if conf.get('experiment_name'):
+        experiment_name = f'{conf['experiment_name']}_{experiment_date}'
     else:
-        # ----------------------------- Local model path ----------------------------- #
+        alg_name = conf['algorithm']['name'].split(':')[-1]
+        experiment_name = f'{alg_name}_{experiment_date}'
+
+    # ---------------------------------------------------------------------------- #
+    #                Load model as starting point if defined                       #
+    # ---------------------------------------------------------------------------- #
+    model_path = None
+    if conf.get('model'):
+        # ---------------------------- Local model path ----------------------------- #
         if conf['model'].get('local_path'):
             model_path = conf['model']['local_path']
 
         # ------------------------ Weights and Bias model path ----------------------- #
         if conf['model'].get('entity'):
-            # Get wandb run or generate a new one
-            if is_wrapped(env, WandBLogger):
-                wandb_run = env.get_wrapper_attr('wandb_run')
-            else:
-                wandb_run = wandb.init()
 
+            api = wandb.Api()
             # Get model path
             artifact_tag = conf['model'].get(
                 'artifact_tag', 'latest')
@@ -172,7 +87,7 @@ try:
                 '/' + conf['model']['artifact_name'] + ':' + artifact_tag
 
             # Download artifact
-            artifact = wandb_run.use_artifact(wandb_path)
+            artifact = api.artifact(wandb_path)
             artifact.download(
                 path_prefix=conf['model']['artifact_path'],
                 root='./')
@@ -190,24 +105,114 @@ try:
             gcloud.read_from_bucket(client, bucket_name, model_path)
             model_path = './' + model_path
 
-        # ---------- Load calibration of normalization for model if required --------- #
-        if conf['model'].get('normalization') and is_wrapped(
-                env, NormalizeObservation):
-            # Update calibrations
-            env.get_wrapper_attr('set_mean')(
-                conf['model']['normalization']['mean'])
-            env.get_wrapper_attr('set_var')(
-                conf['model']['normalization']['var'])
+    # ---------------------------------------------------------------------------- #
+    #                           Environment parameters                              #
+    # ---------------------------------------------------------------------------- #
+    env_params = {}
 
+    # ------- Update env params configuration with env yaml file if exists ------- #
+    if conf.get('env_yaml_config'):
+        with open(conf['env_yaml_config'], 'r') as env_yaml_conf:
+            env_params.update(yaml.load(env_yaml_conf, Loader=yaml.FullLoader))
+
+    # -- Update env params configuration with specified env parameters if exists -- #
+    if conf.get('env_params'):
+        env_params = deep_update(
+            env_params, process_environment_parameters(
+                conf['env_params']))
+
+    # ------------ For this script, the execution name will be updated ----------- #
+    env_params.update({'env_name': experiment_name})
+
+    # ---------------------------------------------------------------------------- #
+    #                            Wrappers definition                               #
+    # ---------------------------------------------------------------------------- #
+    wrappers = {}
+
+    # ------------------ Read wrappers from yaml file if exists ------------------ #
+    if conf.get('wrappers_yaml_config'):
+        with open(conf['wrappers_yaml_config'], 'r') as f:
+            wrappers = yaml.load(f, Loader=yaml.FullLoader)
+
+    # ------ Read wrappers from yaml file and overwrite yaml file if exists ------ #
+    if conf.get('wrappers'):
+        # Update wrappers with the ones defined in the yaml file
+        for wrapper in conf['wrappers']:
+            for wrapper_name, wrapper_arguments in wrapper.items():
+                for name, value in wrapper_arguments.items():
+                    # parse str parameters to sinergym Callable or Objects if
+                    # required
+                    if isinstance(value, str):
+                        if ':' in value:
+                            wrapper_arguments[name] = import_from_path(value)
+            wrappers = deep_update(wrappers, {wrapper_name: wrapper_arguments})
+
+    # ---------------------------------------------------------------------------- #
+    #                Create environment with parameters and wrappers               #
+    # ---------------------------------------------------------------------------- #
+
+    env = create_environment(
+        env_id=conf['environment'],
+        env_params=env_params,
+        wrappers=wrappers)
+
+    # ---------------------------------------------------------------------------- #
+    #                           Defining model (algorithm)                         #
+    # ---------------------------------------------------------------------------- #
+    alg_name = conf['algorithm']['name']
+    alg_cls = import_from_path(alg_name)
+    alg_params = conf['algorithm'].get(
+        'parameters', {'policy': 'MlpPolicy'})
+    alg_params = process_algorithm_parameters(alg_params)
+
+    # --------------------------- Training from scratch -------------------------- #
+    if model_path is None:
+        try:
+            model = alg_cls(env=env, ** alg_params)
+        except NameError:
+            raise NameError(
+                'Algorithm {} does not exists. It must be a valid SB3 algorithm.'.format(alg_name))
+
+    # --------------------- Traning from a pre-trained model --------------------- #
+    else:
         model = None
         try:
-            model = eval(alg_name).load(
+            model = alg_cls.load(
                 model_path)
         except NameError:
             raise NameError(
                 'Algorithm {} does not exists. It must be a valid SB3 algorithm.'.format(alg_name))
 
         model.set_env(env)
+
+    # ---------------------------------------------------------------------------- #
+    #                 Register hyperparameters in wandb if enabled                 #
+    # ---------------------------------------------------------------------------- #
+    if is_wrapped(env, WandBLogger):
+        experiment_params = {
+            'sinergym-version': sinergym.__version__,
+            'python-version': sys.version,
+            'stable-baselines3-version': sb3_version
+        }
+
+        conf['env_params'] = env.get_wrapper_attr('to_dict')()
+        experiment_params.update(conf)
+
+        env.get_wrapper_attr('wandb_run').config.update(experiment_params)
+
+    # ---------------------------------------------------------------------------- #
+    #                              SET UP WANDB LOGGER                             #
+    # ---------------------------------------------------------------------------- #
+    if is_wrapped(env, WandBLogger):
+        # wandb and SB3 logger
+        logger = SB3Logger(
+            folder=None,
+            output_formats=[
+                HumanOutputFormat(
+                    sys.stdout,
+                    max_length=120),
+                WandBOutputFormat()])
+        model.set_logger(logger)
 
     # ---------------------------------------------------------------------------- #
     #       Calculating total training timesteps based on number of episodes       #
@@ -220,28 +225,38 @@ try:
     # ---------------------------------------------------------------------------- #
     callbacks = []
 
-    # Set up Evaluation and best model saving
+    # ---------------------------- EVALUATION CALLBACK --------------------------- #
     if conf.get('evaluation'):
+
+        # ------------ Preparing the evaluation environment configuration ------------ #
+        env_params['env_name'] = experiment_name + '_EVALUATION'
+
+        # By default, the evaluation environment does not use WandBLogger
+        if wrappers:
+            key_to_remove = [
+                key for key in wrappers if 'WandBLogger' in key][0]
+            del wrappers[key_to_remove]
+
+        # ----------------------- Create evaluation environment ---------------------- #
+        eval_env = create_environment(
+            env_id=conf['environment'],
+            env_params=env_params,
+            wrappers=wrappers)
+
+        # ------------------------ Create evaluation callback ------------------------ #
         eval_callback = LoggerEvalCallback(
             eval_env=eval_env,
             train_env=env,
             n_eval_episodes=conf['evaluation']['eval_length'],
             eval_freq_episodes=conf['evaluation']['eval_freq'],
-            deterministic=True)
+            deterministic=True,
+            excluded_metrics=[
+                'episode_num',
+                'length (timesteps)',
+                'time_elapsed (hours)'],
+            verbose=1)
 
         callbacks.append(eval_callback)
-
-    # Set up wandb logger
-    if is_wrapped(env, WandBLogger):
-        # wandb and SB3 logger
-        logger = SB3Logger(
-            folder=None,
-            output_formats=[
-                HumanOutputFormat(
-                    sys.stdout,
-                    max_length=120),
-                WandBOutputFormat()])
-        model.set_logger(logger)
 
     callback = CallbackList(callbacks)
 
